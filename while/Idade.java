ec278a006a5bed0beb21e0c4714da2aab6cb93ca
Nicolas Annibal <coliannibal@gmail.com>
2024-09-20 08:52:57 -0300

    Add files via upload

--- while/Idade.java
+++ while/Idade.java
@@ -1,22 +1,43 @@
 package exercicios;
+
 import java.util.Scanner;
-public class Idade {
-public static void main(String[] args) {
-	Scanner ler=new Scanner(System.in);
-	int idade, ano, i=1, anoNasc;
-	while (i<=6) {
-	System.out.println("digite seu ano de nascimento");
-	anoNasc = ler.nextInt();
-	System.out.println("digite ano atual");
-	ano = ler.nextInt();
-	idade=ano-anoNasc;
-	if (idade<18) {
-		System.out.println("voce e menor de idade");
-	}else {
-		System.out.println("voce e maior de idade");
-	}
-	i=i+1;
-	
-	}
-}
+
+public class Idade{
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+        int anoAtual, anoNascimento, idade;
+        int idadeMaisVelho = Integer.MIN_VALUE;
+        int idadeMaisNovo = Integer.MAX_VALUE;
+        
+        int[] idades = new int[10];
+ 
+        System.out.print("Digite o ano atual: ");
+        anoAtual = scanner.nextInt();
+ 
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite o ano de nascimento do usuário " + (i + 1) + ": ");
+            anoNascimento = scanner.nextInt();
+            idade = anoAtual - anoNascimento;
+ 
+            idades[i] = idade;
+ 
+            if (idade > idadeMaisVelho) {
+                idadeMaisVelho = idade;
+            }
+ 
+            if (idade < idadeMaisNovo) {
+                idadeMaisNovo = idade;
+            }
+        }
+ 
+        System.out.println("\nIdades dos usuários:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Usuário " + (i + 1) + ": " + idades[i] + " anos");
+        }
+ 
+        System.out.println("\nIdade do usuário mais velho: " + idadeMaisVelho + " anos");
+        System.out.println("Idade do usuário mais novo: " + idadeMaisNovo + " anos");
+ 
+        scanner.close();
+    }
 }
